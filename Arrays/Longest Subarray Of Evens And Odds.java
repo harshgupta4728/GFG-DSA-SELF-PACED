551c17f757da002e392599227fb29b9d08b59c2f
HARSH GUPTA <119775951+harshgupta4728@users.noreply.github.com>
2024-06-26 18:12:19 +0530

    Create Longest Subarray Of Evens And Odds.java

---
+++ Arrays/Longest Subarray Of Evens And Odds.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxEvenOdd(int[] arr, int n) {
+        int maxLength = 1;
+        int currentLength = 1;
+        for (int i = 1; i < n; i++) {
+            if ((arr[i] % 2 == 0 && arr[i - 1] % 2 != 0) ||
+                (arr[i] % 2 != 0 && arr[i - 1] % 2 == 0)) {
+                currentLength++;
+            } else {
+                maxLength = Math.max(maxLength, currentLength);
+                currentLength = 1;
+            }
+        }
+
+        maxLength = Math.max(maxLength, currentLength);
+        return maxLength;
+    }
+}
