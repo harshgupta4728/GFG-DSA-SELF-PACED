f2b7278168305ee2d3f3feaf7ca5794fb65917a8
HARSH GUPTA <119775951+harshgupta4728@users.noreply.github.com>
2024-06-28 13:14:45 +0530

    Update Maximum Water Between Two Buildings.java

--- Searching/Maximum Water Between Two Buildings.java
+++ Searching/Maximum Water Between Two Buildings.java
@@ -1,1 +1,26 @@
 
+class Solution
+{
+    // Function to return the maximum water that can be stored.
+    static int maxWater(int height[], int n) 
+    { 
+        if (n < 2) return 0;
+
+        int left = 0, right = n - 1;
+        int maxWater = 0;
+
+        while (left < right) {
+            int minHeight = Math.min(height[left], height[right]);
+            int distance = right - left - 1;
+            maxWater = Math.max(maxWater, minHeight * distance);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxWater;
+    } 
+}
