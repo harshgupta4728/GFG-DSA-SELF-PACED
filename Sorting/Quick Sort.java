397012827d2293315cfed7e4163a559efa3265bb
Harsh Gupta <119775951+harshgupta4728@users.noreply.github.com>
2024-06-30 19:21:57 +0530

    Create Quick Sort.java

---
+++ Sorting/Quick Sort.java
@@ -0,0 +1,43 @@
+class Solution
+{
+    // Function to sort an array using quick sort algorithm.
+    static void quickSort(int arr[], int low, int high)
+    {
+        if (low < high)
+        {
+            // pi is partitioning index, arr[pi] is now at the right place
+            int pi = partition(arr, low, high);
+
+            // Recursively sort elements before partition and after partition
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+    
+    // Function to take last element as pivot, places the pivot element at its correct position in sorted array, and places all smaller (smaller than pivot) to left of pivot and all greater elements to right of pivot
+    static int partition(int arr[], int low, int high)
+    {
+        int pivot = arr[high]; 
+        int i = (low - 1); // index of smaller element
+        for (int j = low; j < high; j++)
+        {
+            // If current element is smaller than or equal to pivot
+            if (arr[j] <= pivot)
+            {
+                i++;
+
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // swap arr[i+1] and arr[high] (or pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+}
