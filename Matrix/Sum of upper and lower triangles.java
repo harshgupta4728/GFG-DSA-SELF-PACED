a633c740abc16b8d0851bfd9b0dad4dbc147c1d8
Harsh Gupta <119775951+harshgupta4728@users.noreply.github.com>
2024-07-01 07:53:09 +0530

    Update Sum of upper and lower triangles.java

--- Matrix/Sum of upper and lower triangles.java
+++ Matrix/Sum of upper and lower triangles.java
@@ -1,1 +1,26 @@
-
+class Solution {
+    // Function to return sum of upper and lower triangles of a matrix.
+    static ArrayList<Integer> sumTriangles(int matrix[][], int n) {
+        int upperSum = 0;
+        int lowerSum = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // Elements in the upper triangle including the diagonal
+                if (i <= j) {
+                    upperSum += matrix[i][j];
+                }
+                // Elements in the lower triangle including the diagonal
+                if (i >= j) {
+                    lowerSum += matrix[i][j];
+                }
+            }
+        }
+        
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(upperSum);
+        result.add(lowerSum);
+        
+        return result;
+    }
+}
