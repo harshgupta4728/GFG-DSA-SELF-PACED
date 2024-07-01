029dd5f20aa8893860daeb4bd510e3820a552357
Harsh Gupta <119775951+harshgupta4728@users.noreply.github.com>
2024-07-01 07:36:09 +0530

    Create Rotate by 90 degree.java

---
+++ Matrix/Rotate by 90 degree.java
@@ -0,0 +1,26 @@
+class Solution {
+    // Function to rotate matrix anticlockwise by 90 degrees.
+    static void rotateby90(int matrix[][], int n) {
+        // Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        // Reverse each row
+        for (int i = 0; i < n; i++) {
+            int start = 0;
+            int end = n - 1;
+            while (start < end) {
+                int temp = matrix[start][i];
+                matrix[start][i] = matrix[end][i];
+                matrix[end][i] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
