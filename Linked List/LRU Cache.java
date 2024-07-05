8bc57c6c15364efb31eff2b18814880d7ccac94b
Harsh Gupta <119775951+harshgupta4728@users.noreply.github.com>
2024-07-05 14:17:39 +0530

    Create LRU Cache.java

---
+++ Linked List/LRU Cache.java
@@ -0,0 +1,22 @@
+class LRUCache {
+    private LinkedHashMap<Integer, Integer> cache;
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new LinkedHashMap<>(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return size() > capacity;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return cache.getOrDefault(key, -1);
+    }
+
+    public void set(int key, int value) {
+        cache.put(key, value);
+    }
+}
