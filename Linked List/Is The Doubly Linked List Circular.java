0d9012a5eedfcec2b373169dd02d23e804866810
Harsh Gupta <119775951+harshgupta4728@users.noreply.github.com>
2024-07-05 15:18:09 +0530

    Create Is The Doubly Linked List Circular.java

---
+++ Linked List/Is The Doubly Linked List Circular.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static boolean isCircular(Node head) {
+        if (head == null) {
+            return false;
+        }
+        
+        Node slow = head;
+        Node fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if (slow == fast) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
